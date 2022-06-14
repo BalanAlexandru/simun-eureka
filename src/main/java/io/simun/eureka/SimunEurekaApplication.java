76d51ccea85ee4ffe439d508416eeff030f0ad34
Alexandru <alexandru.balan@tutanota.com>
2022-06-14 16:48:51 +0300

    Enabling eureka server

--- src/main/java/io/simun/eureka/SimunEurekaApplication.java
+++ src/main/java/io/simun/eureka/SimunEurekaApplication.java
@@ -2,8 +2,10 @@ package io.simun.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SimunEurekaApplication {
 
 	public static void main(String[] args) {
