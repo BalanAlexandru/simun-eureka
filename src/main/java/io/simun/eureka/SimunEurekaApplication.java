595be0d502fb0dec4f36dd0085c59d0c25f875bb
Alexandru <alexandru.balan@tutanota.com>
2022-06-14 11:29:11 +0300

    Initial commit

---
+++ src/main/java/io/simun/eureka/SimunEurekaApplication.java
@@ -0,0 +1,13 @@
+package io.simun.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimunEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimunEurekaApplication.class, args);
+	}
+
+}
